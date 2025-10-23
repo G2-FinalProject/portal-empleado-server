140345fad1812162875fdea191dfb011c3550b96
gabriela-her <ari.ghb@gmail.com>
2025-10-23 12:28:43 +0200

    feat(types): create User interface and creation attributes for Sequelize model (#13)

---
+++ src/types/userInterface.ts
@@ -0,0 +1,16 @@
+export interface UserAttributes {
+  id?: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password_hash: string;
+  role_id: number;
+  department_id: number;
+  region: string;
+  city: string;
+  available_days: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export type UserCreationAttributes = Omit<UserAttributes, "id" | "created_at" | "update_at">;
