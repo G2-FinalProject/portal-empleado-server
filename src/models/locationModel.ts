2d43d9b191b5e16b991e6f88e7f9fe6cf12ffcab
gabriela-her <ari.ghb@gmail.com>
2025-10-27 11:14:25 +0100

    fix(models): fix location name with underscore (#13)

--- src/models/locationModel.ts
+++ src/models/locationModel.ts
@@ -1,22 +1,26 @@
 import { Table, Column, Model, DataType, PrimaryKey, AutoIncrement, AllowNull, HasMany } from 'sequelize-typescript';
 import { User } from './userModel.js';
 import { Holiday } from './holidayModel.js';
+import type { LocationAttributes, LocationCreationAttributes } from '../types/locationInterface.js';
 
-@Table({ tableName: 'locations', timestamps: false })
+@Table({ tableName: 'locations', timestamps: false, underscored: true })
 
-export class Location extends Model<Location> {
-  @PrimaryKey 
-  @AutoIncrement 
-  @Column(DataType.INTEGER)
-  declare id: number;
+export class Location 
+extends Model<LocationAttributes, LocationCreationAttributes>
+implements LocationAttributes {
+    @PrimaryKey
+    @AutoIncrement
+    @Column(DataType.INTEGER)
+    declare id: number;
 
-  @AllowNull(false)
-  @Column({ type: DataType.STRING(255), field: 'location_name' })
-  declare locationName: string;
+    @AllowNull(false)
+    @Column({
+        type: DataType.STRING(255)    })
+    declare location_name: string;
 
-  @HasMany(() => User) 
-  declare users: User[];
-  
-  @HasMany(() => Holiday) 
-  declare holidays: Holiday[];
+    @HasMany(() => User)
+    declare users: User[];
+
+    @HasMany(() => Holiday)
+    declare holidays: Holiday[];
 }
