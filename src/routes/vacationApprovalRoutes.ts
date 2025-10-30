0825b77b84d6886def08b33fe18f12cd56d6dcbf
MaryoriCruz <maryori.eguizabal@gmail.com>
2025-10-30 08:46:46 -0500

    feat(middleware): apply authentication and authorization to all route modules (#8) �️
    - Integrated isAuthenticated and hasRole middleware in Role, VacationRequest and VacationApproval routes
    - Restricted Role routes to Admin (role_id: 1)
    - Restricted VacationRequest routes to any authenticated user (roles 1, 2, 3)
    - Restricted VacationApproval routes to Admin or Manager (roles 1, 2)
    - Added validationErrorHandler middleware to ensure proper request validation
    - Confirmed endpoints tested successfully in Postman (200 OK responses)

--- src/routes/vacationApprovalRoutes.ts
+++ src/routes/vacationApprovalRoutes.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { decideVacationRequest } from "../controllers/VacationApprovalController.js";
 import { decideVacationRequestRules } from "../validators/vacationApprovalValidator.js";
+import { handleValidationErrors } from "../middlewares/validationErrorHandler.js";
+import { isAuthenticated, hasRole } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// ✅ Ruta para aprobar o rechazar una solicitud
-router.put("/:id/:action", decideVacationRequestRules, decideVacationRequest);
+const checkManagerOrAdmin = [isAuthenticated, hasRole(1,2)];
+
+
+router.put("/:id/:action", checkManagerOrAdmin, decideVacationRequestRules, handleValidationErrors, decideVacationRequest);
 
 export default router;
