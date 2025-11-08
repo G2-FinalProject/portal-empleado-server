0b92ced76d5c66c24a63c06debec028a985aeef5
gabriela-her <ari.ghb@gmail.com>
2025-11-08 23:37:57 +0100

    chore(config): add test environment variables and dotenv loading logic (#51)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+dotenv.config({ path: envFile });
+
 export default {
   development: {
     database: process.env['DB_NAME']!,
@@ -10,6 +13,16 @@ export default {
     port: 3306,
     dialect: process.env['DB_DIALECT'] as 'mysql',
   },
+
+  test: {  
+    database: process.env['DB_NAME']!,
+    username: process.env['USER_DB']!,
+    password: process.env['PASSWORD_DB']!,
+    host: process.env['HOST']!,
+    port: 3306,
+    dialect: process.env['DB_DIALECT'] as 'mysql',
+  },
+
   jwt: {
     jwtSecret: process.env['JWT_SECRET'] as string,
     jwtExpires: process.env['JWT_EXPIRES'] as '7d',
